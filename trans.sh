46957c8b16f5012b3517cc37cbcae2fa34144299
zjqi <1696808449@qq.com>
2020-03-23 21:12:39 +0800

    Basic file for t21

---
+++ trans.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+SENSOR="sc3235-t21"
+
+if mountpoint -q ~/nfswork/mnt
+then
+    echo "mounted"
+else
+    echo "not mounted"
+    mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ~/nfswork/mnt
+fi
+
+make clean && make S=${SENSOR}
+cp ${SENSOR}.bin ~/nfswork/mnt/ -vf                                                                                                   
+md5sum ${SENSOR}.bin
