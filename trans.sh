ad2e7fc18a7ca53d3c7c111149811d074b808e55
zjqi <zhengjian.qi@ingenic.com>
2020-10-27 15:52:30 +0800

    first submit

--- trans.sh
+++ trans.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ####Select the sensor####
-SENSOR="jxf63-t21"
+SENSOR="jxh62-t21"
 #####NFS OR TFTP##Choose the upload method#
 TOOL="NFS"
 NFS_MY_PATH=/home/zjqi/nfsroot/mnt
@@ -10,34 +10,34 @@ TFTP_WORK_PATH=/home/zjqi/net/tftproot
 
 make clean && make S=${SENSOR}
 
-if [ ${TOOL} = "NFS" ];then
-    if [ -d "${NFS_MY_PATH}" ];then
-        if mountpoint -q ${NFS_MY_PATH};then
-            echo "mounted"
-        else
-            echo "not mounted"
-            mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ${NFS_MY_PATH}/
-        fi
-    
+#if [ ${TOOL} = "NFS" ];then
+#    if [ -d "${NFS_MY_PATH}" ];then
+#        if mountpoint -q ${NFS_MY_PATH};then
+#            echo "mounted"
+#        else
+#            echo "not mounted"
+#            mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ${NFS_MY_PATH}/
+#        fi
+#    
         cp ${SENSOR}.bin ${NFS_MY_PATH}/ -vf                                       
-
-    elif [ -d "${NFS_WORK_PATH}" ];then
-        if mountpoint -q ${NFS_WORK_PATH};then
-            echo "mounted"
-        else
-            echo "not mounted"
-            mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ${NFS_WORK_PATH}/
-        fi
-
-        cp ${SENSOR}.bin ${NFS_WORK_PATH}/ -vf                                                                                                   
-    fi
-
-elif [ ${TOOL} = "TFTP" ];then
-    if [ -d "${TFTP_MY_PATH}" ];then
-        cp ${SENSOR}.bin ${TFTP_MY_PATH}/ -vf 
-    elif [ -d "${TFTP_WORK_PATH}" ];then
-        cp ${SENSOR}.bin ${TFTP_WORK_PATH}/ -vf
-    fi
-fi
+#
+#    elif [ -d "${NFS_WORK_PATH}" ];then
+#        if mountpoint -q ${NFS_WORK_PATH};then
+#            echo "mounted"
+#        else
+#            echo "not mounted"
+#            mount -o nolock -t nfs 193.169.4.2:/home_b/nfsroot/zjqi ${NFS_WORK_PATH}/
+#        fi
+#
+#        cp ${SENSOR}.bin ${NFS_WORK_PATH}/ -vf                                                                                                   
+#    fi
+#
+#elif [ ${TOOL} = "TFTP" ];then
+#    if [ -d "${TFTP_MY_PATH}" ];then
+#        cp ${SENSOR}.bin ${TFTP_MY_PATH}/ -vf 
+#    elif [ -d "${TFTP_WORK_PATH}" ];then
+#        cp ${SENSOR}.bin ${TFTP_WORK_PATH}/ -vf
+#    fi
+#fi
 
 md5sum ${SENSOR}.bin
